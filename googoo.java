156ef02e13da1e13f2adbdf050a2a41e80b8bd6f
woochul17 <106084040+woochul17@users.noreply.github.com>
2022-05-23 21:53:31 +0900

    Create googoo.java

---
+++ googoo.java
@@ -0,0 +1,11 @@
+public class Main1 {
+    public static void main(String[]args){
+        for (int i = 1; i <10 ; i++) {
+            for (int j = 1; j <10 ; j++) {
+                System.out.println(String.format("0%dx0%d=0%d",i,j,i*j));
+
+            }
+
+        }
+
+    }
